c6e9ba5676724c20ab28567fe5c7e2c3a0a4b162
donny <donny.vallejo@gmail.com>
2017-03-29 00:27:36 -0700

    added the securityQuestion object and content. the rest is really hard for me. i suck :/

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,16 @@
 console.log("security_questions.js is loaded");
+
+ var securityQuestions = [
+ 	{
+ 		question: "How do you Js?",
+ 		expectedAnswer: "I wish i knew"
+ 	},
+ 	{
+ 		question: "where do you live?",
+ 		expectedAnswer: "141 Avalon dr. Daly city"
+ 	},
+ 	{
+ 		question: "Where in the world is carmen sandiego",
+ 		expectedAnswer: "Here?!?!"
+ 	}
+ ]
